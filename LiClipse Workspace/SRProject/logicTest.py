260024540772e7704591f893deb65217aab31276
Kyle N <kyle@treez.io>
2016-12-07 07:22:26 -0800

    Committing changes

---
+++ LiClipse Workspace/SRProject/logicTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sensorLibrary
+from nanpy import Arduino
+from nanpy import serial_manager
+from time import sleep
+import smbus
+import time
+import math
+s = sensorLibrary
+
